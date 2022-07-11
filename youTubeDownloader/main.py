a16664186689d21a4b3f2f3770fd4ed202c017cd
JhonatanClarindo <jhonatan.silva@lightbase.com.br>
2022-07-11 10:58:30 -0300

    Changes youtube downloader

--- youTubeDownloader/main.py
+++ youTubeDownloader/main.py
@@ -1,17 +1,12 @@
 try:
     from pytube import YouTube
-    from pytube import Playlist
 except Exception as e:
     print("error". format(e))
 
 
-# url = input(str('Url for download: '))
-url = 'https://youtu.be/3Tb0NWTVtqE'
+url = input(str('Url for download: '))
+
 
 video = YouTube(url).streams.first().download()
-
-# stream = video.streams.get_highest_resolution()
-
-# stream.download(output_path='/home/jhonatan/www/estudos')
 
 print("Download concluido com sucesso!")
